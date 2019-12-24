7b0fcca27cc47a2b08019d56d53b3c6994edb17e
xcl(徐程林) <xcl@winning.com.cn>
2019-12-24 14:25:38 +0800

    调整演示

--- spring-boot-actuator/src/main/java/top/felixfly/springboot/springbootdemo/actuator/SpringBootActuatorApplication.java
+++ spring-boot-actuator/src/main/java/top/felixfly/springboot/springbootdemo/actuator/SpringBootActuatorApplication.java
@@ -2,6 +2,9 @@ package top.felixfly.springboot.springbootdemo.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import javax.swing.*;
 
 /**
  *
